b38a88aff7384cff1624ad6e23b200fd1e761691
Branzil <branzil.nbj@gmail.com>
2022-06-20 23:13:01 +0530

    third - 3

--- src/component/navbar/navbar.jsx
+++ src/component/navbar/navbar.jsx
@@ -7,7 +7,7 @@ const  Navbar = ()=> {
             <div className='Navbar sticky' >
                 <nav className="navbar navbar-expand-lg bg-light">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="#">News Monke</a>
+                        <button className="navbar-brand" >News Monke</button>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
